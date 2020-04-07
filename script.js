89fddc42b376beaf926fde2733e64d226151dd08
nicholasb.za@gmail.com <nicholasb.za@gmail.com>
2020-04-07 22:01:21 +0200

    Made progress with building a snake on canvas, accurate movement needs work, but good progress made

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 'use strict'
+const ctx = document.getElementById(canvasId).getContext('2d');
 const totalWidth = 10000;
 const totalHeight = 10000;
 
 const centerX = (totalWidth / 2);
 const centerY = (totalHeight / 2);
+const clientCenterX = (window.innerWidth / 2);
+const clientCenterY = (window.innerHeight / 2);
+
 const canvasId = "canvas";
-const velocity = 1;
-let mouseClientX = centerX;
-let mouseClientY = centerY;
+const velocity = 2;
+const fps = 24;
+
+let mouseClientX = clientCenterX;
+let mouseClientY = clientCenterY;
 let movingUp = (window.innerWidth / 2) < mouseClientX;
 let movingLeft = (window.innerHeight / 2) < mouseClientY;
 let snakes = [];
@@ -18,34 +24,69 @@ function setCanvasSize() {
     canvas.height = totalHeight;
 }
 
-function createSnake(x, y) {
-    var circle = new Path2D();
-    circle.arc(x, y, 30, 0, 2 * Math.PI);
-    circle.fillStyle = "blue";
-    return circle;
+function segment(x, y){
+    this.x = x;
+    this.y = y;
+
+    this.draw = function(isHead) {
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.arc(segment.x, segment.y, 30, 0, 2 * Math.PI);
+        if(isHead){
+            ctx.fillStyle = 'green';
+        }
+        else {
+            ctx.strokeStyle = 'brown';
+            ctx.fillStyle = 'blue';
+        }
+        ctx.fill();
+    }
 }
 
+function snake(segments) {
+    this.segments = segments;
+    this.move = function(x, y) {
+        this.segments.pop();
+        let head = this.segments[0];
+        this.segments.push(new segment(head.x + x, head.y + y))
+    }
+    this.draw = function() {
+        segments[0].draw(true);//head
+        for (let i = 1; i < segments.length; i++) {
+            segments[index].draw(false);
+        }
+    }
+};
+
 function drawAll() {
-    let ctx = document.getElementById(canvasId).getContext('2d');
-    ctx.save();
+    ctx.globalCompositeOperation = 'destination-over';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    const yposition = (mouseClientY - clientCenterY);
+    const xposition = (mouseClientX - clientCenterX);
+    var gradient = yposition / xposition;
+
+    if(Number.isNaN(gradient)) {
+        gradient = 1;
+    }
+
+    let moveX = velocity / Math.sqrt(Math.pow(gradient, 2) + 1);
+    let moveY = Math.sqrt(Math.pow(velocity, 2) - Math.pow(moveX, 2));
+
+    //invert because of screen coordinates being upsite down.
+    moveX *= -1;
+    moveY *= -1;
+
+    snakeItem.x += moveX;
+    snakeItem.y += moveY;
+
+    //will need to change when remote snakes are included.
     for (let i = 0; i < snakes.length; i++) {
-        ctx.restore();
-        const snake = snakes[i];
-        let moveX = velocity;
-        let moveY = velocity;
+        const snakeItem = snakes[i];
+        snakeItem.move(moveX, moveY);
+        snakeItem.draw();
 
-        //set movement direction
-        if(movingLeft) {
-            moveX = moveX * -1;
-        }
-        if(movingUp) {
-            moveY = moveY * -1;
-        }
-
-        ctx.translate(moveX, moveY);
-        ctx.fill(snake);
-        window.scrollBy(moveX, moveY);
+        window.scrollTo(snakeItem.x, snakeItem.y);
     }
 
     window.requestAnimationFrame(drawAll);
@@ -55,14 +96,24 @@ function onLoad() {
     setCanvasSize();
     document.onmousemove = (e) => showCoOrdinates(e);
     window.scrollTo(centerX - window.innerWidth / 2, centerY - window.innerHeight / 2);
-    snakes.push(createSnake(centerX, centerY));
+
+    var segments = [];
+    for (let i = 0; i < 5; i++) {
+        segments.push(new segment(centerX, centerY + i));
+    }
+    snakes.push(new snake(segments));
+
+    //add fps delay on this call
     window.requestAnimationFrame(drawAll);
 }
 
 function showCoOrdinates(event) {
     var span = document.getElementById("coords");
-    span.innerHTML = "Page:(" + event.pageX + "," + event.pageY + "), Client:(" + event.clientX + "," + event.clientY + ")"; //can use clientX, clientY for in view co-ordinates.
+    span.innerHTML = "Page:(" + event.pageX + "," + event.pageY + "), Client:(" + event.clientX + "," + event.clientY + ")";
 
     movingUp = event.clientY < (window.innerHeight / 2);
     movingLeft = event.clientX < (window.innerWidth / 2);
+
+    mouseClientX = event.clientX;
+    mouseClientY = event.clientY;
 }
