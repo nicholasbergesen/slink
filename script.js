ce29fc38b7b1454b7892b71582c5a24d55bb0c8e
nicholasb.za@gmail.com <nicholasb.za@gmail.com>
2020-04-08 20:04:13 +0200

    Working following mouse, based off center screen coordinate

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
-const totalWidth = 10000;
-const totalHeight = 10000;
+const totalWidth = 3000;
+const totalHeight = 3000;
 
 const centerX = (totalWidth / 2);
 const centerY = (totalHeight / 2);
@@ -8,7 +8,8 @@ const clientCenterX = (window.innerWidth / 2);
 const clientCenterY = (window.innerHeight / 2);
 
 const canvasId = "canvas";
-const velocity = 2;
+const velocity = 10;
+const segmentRadius = 30;
 const fps = 24;
 var ctx = document.getElementById(canvasId).getContext('2d');
 
@@ -24,60 +25,68 @@ function setCanvasSize() {
     canvas.height = totalHeight;
 }
 
-function segment(x, y){
+function segment(x, y) {
     this.x = x;
     this.y = y;
 
     this.draw = function(isHead) {
         ctx.beginPath();
         ctx.moveTo(this.x, this.y);
-        ctx.arc(segment.x, segment.y, 30, 0, 2 * Math.PI);
+        ctx.arc(this.x, this.y, segmentRadius, 0, 2 * Math.PI);
         if(isHead){
-            ctx.fillStyle = 'green';
+            ctx.fillStyle = "green";
         }
         else {
-            ctx.strokeStyle = 'brown';
-            ctx.fillStyle = 'blue';
+            ctx.strokeStyle = "brown";
+            ctx.fillStyle = "blue";
         }
         ctx.fill();
+        ctx.stroke();
     }
 }
 
 function snake(segments) {
     this.segments = segments;
     this.move = function(x, y) {
-        this.segments.pop();
+        for (let i = this.segments.length - 1; i > 0; i--) {
+            this.segments[i] = this.segments[i - 1];            
+        }
         let head = this.segments[0];
-        this.segments.push(new segment(head.x + x, head.y + y))
+        head.x += x;
+        head.y += y;
     }
     this.draw = function() {
         let head = segments[0];
-        head.draw(true);//head
         for (let i = 1; i < segments.length; i++) {
             segments[i].draw(false);
         }
-        window.scrollTo(head.x, head.y);
+        head.draw(true);//head
+        window.scrollTo(head.x - clientCenterX, head.y - clientCenterY);
     }
 };
 
 function drawAll() {
-    ctx.globalCompositeOperation = 'destination-over';
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     const yposition = (mouseClientY - clientCenterY);
     const xposition = (mouseClientX - clientCenterX);
+
     var gradient = yposition / xposition;
 
     if(Number.isNaN(gradient)) {
-        gradient = 1;
+        gradient = 0;
     }
 
     let moveX = velocity / Math.sqrt(Math.pow(gradient, 2) + 1);
     let moveY = Math.sqrt(Math.pow(velocity, 2) - Math.pow(moveX, 2));
 
     //invert because of screen coordinates being upsite down.
-    moveX *= -1;
-    moveY *= -1;
+    if(movingUp) {
+        moveY *= -1;
+    }
+    if(movingLeft) {
+        moveX *= -1;
+    }
 
     //will need to change when remote snakes are included.
     for (let i = 0; i < snakes.length; i++) {
@@ -92,16 +101,16 @@ function drawAll() {
 function onLoad() {
     setCanvasSize();
     document.onmousemove = (e) => showCoOrdinates(e);
-    window.scrollTo(centerX - window.innerWidth / 2, centerY - window.innerHeight / 2);
+    window.scrollTo(centerX - clientCenterX, centerY - clientCenterY);
 
     var segments = [];
     for (let i = 0; i < 5; i++) {
-        segments.push(new segment(centerX, centerY + i));
+        segments.push(new segment(centerX, centerY + (i * segmentRadius)));
     }
     snakes.push(new snake(segments));
-
     //add fps delay on this call
-    window.requestAnimationFrame(drawAll);
+    //window.requestAnimationFrame(drawAll);
+    drawAll();
 }
 
 function showCoOrdinates(event) {
@@ -109,8 +118,8 @@ function showCoOrdinates(event) {
     span.innerHTML = "Page:(" + event.pageX + "," + event.pageY + "), Client:(" + event.clientX + "," + event.clientY + ")";
 
     //todo: remove this use gradient.
-    movingUp = event.clientY < (window.innerHeight / 2);
-    movingLeft = event.clientX < (window.innerWidth / 2);
+    movingUp = event.clientY < clientCenterY;
+    movingLeft = event.clientX < clientCenterX;
 
     mouseClientX = event.clientX;
     mouseClientY = event.clientY;
