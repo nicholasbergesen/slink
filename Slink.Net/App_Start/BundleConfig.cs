e4c8761318f4caaeaa525b67dee570ab78a184d8
nicholasb.za@gmail.com <nicholasb.za@gmail.com>
2020-04-10 17:19:51 +0200

    Change to vs solution, added web api for server comms to allow multiplayer

---
+++ Slink.Net/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Slink.Net
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js").Include(
+                        "~/Scripts/slink.js"));
+
+            bundles.Add(new StyleBundle("~/css").Include(
+                      "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
